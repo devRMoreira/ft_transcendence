b9e91a3f1a516493bb42c489afd0d8676490daa6
Ricardo Moreira <devrmoreira@gmail.com>
2026-09-20 14:47:20 +0000

    Added Navbar and updated homepage

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,29 @@
-import AppThemeProvider from "./theme-provider";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import AppThemeProvider from "./theme-provider"
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter"
 import { SessionProvider } from "next-auth/react"
-import { auth } from "@/auth";
+import { auth } from "@/auth"
+import { Navbar } from "@/components/NavBar"
 
 export const metadata = {
-  title: "CardGame",
-  description: "CardGame Description",
-};
+	title: "CardGame",
+	description: "CardGame Description",
+}
 
 export default async function RootLayout({ children }) {
+	const session = await auth()
 
-  const session = await auth()
-
-  return (
-    <html lang="en" >
-      <body>
-        <SessionProvider session={session}>
-          <AppRouterCacheProvider>
-            <AppThemeProvider>
-              {children}
-            </AppThemeProvider>
-          </AppRouterCacheProvider>
-        </SessionProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<SessionProvider session={session}>
+					<AppRouterCacheProvider>
+						<AppThemeProvider>
+							<Navbar/>
+							{children}
+						</AppThemeProvider>
+					</AppRouterCacheProvider>
+				</SessionProvider>
+			</body>
+		</html>
+	)
 }
--- proxy.js
+++ proxy.js
@@ -1,18 +1,16 @@
 import { auth } from "@/auth"
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname === "/")
-    return Response.redirect(
-      new URL("/signin", req.nextUrl.origin)
-    )
-  else if(req.auth && (req.nextUrl.pathname === "/signin" || req.nextUrl.pathname === "/signup"))
-      return Response.redirect(
-    new URL("/", req.nextUrl.origin))
-  })
+	const isLoggedIn = Boolean(req.auth)
+	const { pathname } = req.nextUrl
+	const publicRoutes = ["/signin", "/signup"]
+	const isPublicRoute = publicRoutes.includes(pathname)
 
+	if (!isLoggedIn && !isPublicRoute)
+		return Response.redirect(new URL("/signin", req.nextUrl.origin))
+	if (isLoggedIn && isPublicRoute) return Response.redirect(new URL("/", req.nextUrl.origin))
+})
 
-
-// paths to exclude proxy from running on
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// }
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
