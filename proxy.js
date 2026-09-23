c593001f406f4a1746538bc2eb28ace3890eb574
Rodrigo Faria <rodrigofal44@gmail.com>
2026-09-23 15:40:15 +0100

    Authenticated users can now view privacy and terms again.

--- proxy.js
+++ proxy.js
@@ -5,10 +5,11 @@ export default auth((req) => {
 	const { pathname } = req.nextUrl
 	const publicRoutes = ["/signin", "/signup", "/terms", "/privacy"]
 	const isPublicRoute = publicRoutes.includes(pathname)
+	const isAuthRoute = ["/signin", "/signup"].includes(pathname)
 
 	if (!isLoggedIn && !isPublicRoute)
 		return Response.redirect(new URL("/signin", req.nextUrl.origin))
-	if (isLoggedIn && isPublicRoute) return Response.redirect(new URL("/", req.nextUrl.origin))
+	if (isLoggedIn && isAuthRoute) return Response.redirect(new URL("/", req.nextUrl.origin))
 })
 
 export const config = {
